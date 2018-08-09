a2b8725669cf01848d4dd2f1cd5aed1595342983
Daniel Phillips <dan.phillips@aljex.com>
2018-08-09 14:39:37 -0600

    Add attachment relationships

--- app/controllers/temp_resources_controller.rb
+++ app/controllers/temp_resources_controller.rb
@@ -69,6 +69,6 @@ class TempResourcesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def temp_resource_params
-      params.fetch(:temp_resource, {})
+      params.fetch(:temp_resource, :uploads, {})
     end
 end
--- app/models/temp_resource.rb
+++ app/models/temp_resource.rb
@@ -1,2 +1,3 @@
 class TempResource < ApplicationRecord
+  has_many_attached :uploads
 end
